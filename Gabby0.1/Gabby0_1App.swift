6b2d7ead76da728720dfb9ef885248762502b43c
BigSlikTobi <tobias.latta@hotmail.de>
2024-10-10 12:32:07 +0200

    Initial Commit

---
+++ Gabby0.1/Gabby0_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Gabby0_1App.swift
+//  Gabby0.1
+//
+//  Created by Tobias Latta on 10.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Gabby0_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gabby0.1Tests/Gabby0_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Gabby0_1Tests.swift
+//  Gabby0.1Tests
+//
+//  Created by Tobias Latta on 10.10.24.
+//
+
+import Testing
+@testable import Gabby0_1
+
+struct Gabby0_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
